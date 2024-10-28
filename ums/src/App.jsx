cb0d0aa4647b7470c0c57febe19fdd105067e15e
anittanelson <anittanelson40@gmail.com>
2024-10-28 09:55:58 +0530

    task

---
+++ ums/src/App.jsx
@@ -0,0 +1,25 @@
+
+// import Login from "./components/ums/pages/login.css";
+// import "./Components/ums/pages/login.css"
+// import Forgot from "./Components/ums/forget/forget"
+// import "./Components/ums/forget/forgot.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Reset from "./Components/ums/reset/Reset"
+// import "./Components/ums/reset/Reset.css"
+
+// import Update from "./Components/ums/update/Update";
+// import "./Components/ums/update/update.css"
+function App() {
+
+  return (
+    <>
+      {/* <Login /> */}
+      {/* <Forgot /> */}
+      {/* < Reset /> */}
+      {/* <Update /> */}
+
+    </>
+  )
+}
+
+export default App
